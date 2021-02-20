fc95515209760ad59706c0b9128dbed484d99bc5
Hernan Yupanqui <hernan.yupanqui.prieto@gmail.com>
2021-02-20 15:42:15 -0500

    testing 4

--- app/Controllers/Inicio.php
+++ app/Controllers/Inicio.php
@@ -357,7 +357,7 @@ class Inicio extends BaseController {
 			$mail->AllowEmpty = true;   
 
 			// De: 
-			$mail->setFrom('reenviadorweb@innomedic.pe',  $usuario.'-'.$identification_number);
+			$mail->setFrom('localhost',  $usuario.'-'.$identification_number);
 
 			// Configurando el boton de responder
 			$mail->addReplyTo($email, 'Pedido de Cotizacion - Pagina web  Web innomedic');
