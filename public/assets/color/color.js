ba94ec34f0892cf74ef3b864c18f013bc45228b2
Hernan <hernan.yupanqui.prieto@gmail.com>
2021-06-14 19:14:44 -0500

    solucionando problemas de cache 7

--- public/assets/color/color.js
+++ public/assets/color/color.js
@@ -1,11 +1,11 @@
 $(function () {
-	$('head').append(`<link rel="stylesheet" href="public/assets/css/style-color-5.css?v=${Math.floor(Math.random() * 1000000)}" rel="stylesheet">`);
+	//$('head').append(`<link rel="stylesheet" href="public/assets/css/style-color-5.css?v=${Math.floor(Math.random() * 1000000)}" rel="stylesheet">`);
 	$('.js-settings-panel-btn').on('click',function (e){
 		var $this = $(this).parent('.js-settings-panel-item');
 		$('.js-settings-panel-item').not($this).removeClass('opened');
 		$this.toggleClass('opened');
 	})
-	$('.js-swatch-color').on('click',function (e){
+	/*$('.js-swatch-color').on('click',function (e){
 		var color = $(this).attr('data-color');
 		if(color != undefined){
 			$('link[href*="public/assets/css/style-"]').not('link[href*="public/assets/css/style-rtl"]').attr('href','public/assets/css/style-color-'+color+'.css');
@@ -15,5 +15,5 @@ $(function () {
 		$('.js-swatch-color').removeClass('active');
 		$(this).toggleClass('active');
 		e.preventDefault();
-	});
+	});*/
 })
